1775a8be57bacf564a5832d60b91d006ae2c4cab
Nilton-Linux <niltonff@hotmail.com>
2022-11-28 09:05:09 -0300

    teste de voltar o rash

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando Sitema de Gerenciamento de Jogos")
 console.log("aplicação está se conectando ao banco de dados")
-console.log("teste de comit")
-console.log("teste de comit 2")
